48c4ab32f3ce62a28aba5053082a1f42c18bff8b
Tom Dudley <TomDudley92@users.noreply.github.com>
2019-03-18 15:41:28 +0000

    Refactored repo to team standards

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,17 +1,19 @@
-resolvers += Resolver.url("HMRC Sbt Plugin Releases", url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(
-  Resolver.ivyStylePatterns)
+resolvers += Resolver.url("HMRC Sbt Plugin Releases", url("https://dl.bintray.com/hmrc/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
 
 resolvers += "HMRC Releases" at "https://dl.bintray.com/hmrc/releases"
 
-resolvers += Resolver.typesafeRepo("releases")
+resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"
 
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.13.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.14.0")
-
-addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "1.16.0")
-
-addSbtPlugin("uk.gov.hmrc" % "sbt-artifactory" % "0.17.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "1.15.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "1.3.0")
 
+addSbtPlugin("uk.gov.hmrc" % "sbt-artifactory" % "0.17.0")
+
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.19")
+
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.9.0")
+
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.0")
--- test/controllers/HelloWorldControllerSpec.scala
+++ test/controllers/HelloWorldControllerSpec.scala
@@ -1,4 +1,20 @@
-package uk.gov.hmrc.submitvatreturnfrontend.controllers
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
 
 import org.scalatest.{Matchers, WordSpec}
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
@@ -7,7 +23,7 @@ import play.api.i18n.{DefaultLangs, DefaultMessagesApi}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import play.api.{Configuration, Environment}
-import uk.gov.hmrc.submitvatreturnfrontend.config.AppConfig
+import config.FrontendAppConfig
 
 
 class HelloWorldControllerSpec extends WordSpec with Matchers with GuiceOneAppPerSuite {
@@ -17,9 +33,9 @@ class HelloWorldControllerSpec extends WordSpec with Matchers with GuiceOneAppPe
   val configuration = Configuration.load(env)
 
   val messageApi = new DefaultMessagesApi(env, configuration, new DefaultLangs(configuration))
-  val appConfig = new AppConfig(configuration, env)
+  val appConfig = new FrontendAppConfig(configuration, env)
 
-  val controller = new HelloWorld(messageApi, appConfig)
+  val controller = new HelloWorldController(messageApi, appConfig)
 
   "GET /" should {
     "return 200" in {
