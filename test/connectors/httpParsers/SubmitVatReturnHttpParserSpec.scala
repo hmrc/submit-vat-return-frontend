29026f546294edd14c11045210a10390c593a3be
Miranda Hawkes <17408278+miranda-hawkes@users.noreply.github.com>
2019-05-28 11:08:46 +0100

    Added submit VAT return parser

---
+++ test/connectors/httpParsers/SubmitVatReturnHttpParserSpec.scala
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors.httpParsers
+
+import base.BaseSpec
+import connectors.httpParsers.SubmitVatReturnHttpParser.SubmitVatReturnReads
+import models.SubmissionSuccessModel
+import models.errors.{ServerSideError, UnexpectedJsonFormat}
+import play.api.http.Status._
+import play.api.libs.json.{JsObject, Json}
+import uk.gov.hmrc.http.HttpResponse
+
+class SubmitVatReturnHttpParserSpec extends BaseSpec {
+
+  val correctResponseJson: JsObject = Json.obj(
+    "formBundleNumber" -> "12345"
+  )
+
+  val incorrectResponseJson: JsObject = Json.obj(
+    "nope" -> "nope"
+  )
+
+  val errorResponseJson: JsObject = Json.obj(
+    "code" -> "SERVICE_UNAVAILABLE",
+    "reason" -> "oh no"
+  )
+
+  "CustomerDetailsReads" when {
+
+    "response is 200" when {
+
+      "response body is in expected JSON format" should {
+
+        val httpResponse = HttpResponse(
+          OK,
+          Some(correctResponseJson)
+        )
+
+        val expectedResult = SubmissionSuccessModel(
+          "12345"
+        )
+
+        val result = SubmitVatReturnReads.read("", "", httpResponse)
+
+        "return a success model" in {
+          result shouldBe Right(expectedResult)
+        }
+      }
+
+      "response body is not in expected JSON format" should {
+
+        val httpResponse = HttpResponse(
+          OK,
+          Some(incorrectResponseJson)
+        )
+
+        val expectedResult = UnexpectedJsonFormat
+
+        val result = SubmitVatReturnReads.read("", "", httpResponse)
+
+        "return an UnexpectedJsonFormat model" in {
+          result shouldBe Left(expectedResult)
+        }
+      }
+    }
+
+    "response is not 200" should {
+
+      val httpResponse = HttpResponse(
+        SERVICE_UNAVAILABLE,
+        Some(errorResponseJson)
+      )
+
+      val expectedResult = ServerSideError(SERVICE_UNAVAILABLE.toString, "Received downstream error when submitting VAT return.")
+
+      val result = SubmitVatReturnReads.read("", "", httpResponse)
+
+      "return a ServerSideError model" in {
+        result shouldBe Left(expectedResult)
+      }
+    }
+  }
+}
