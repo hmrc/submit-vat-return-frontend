bca1eefa0000dcff9a7f12a14d641fa4d1a89821
Miranda Hawkes <17408278+miranda-hawkes@users.noreply.github.com>
2019-05-28 14:33:08 +0100

    Added vat returns service

---
+++ test/services/VatReturnsServiceSpec.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import java.time.LocalDate
+
+import base.BaseSpec
+import connectors.{VatObligationsConnector, VatReturnsConnector}
+import connectors.httpParsers.ResponseHttpParsers.HttpGetResult
+import models.errors.UnexpectedJsonFormat
+import models.vatReturnSubmission.{SubmissionModel, SubmissionSuccessModel}
+import models.{VatObligation, VatObligations}
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class VatReturnsServiceSpec extends BaseSpec {
+
+  val mockConnector: VatReturnsConnector = mock[VatReturnsConnector]
+  val service = new VatReturnsService(mockConnector)
+
+  "Calling .submitVatReturn" when {
+
+    val submissionModel = SubmissionModel(
+      periodKey = "19AA",
+      vatDueSales = 10.01,
+      vatDueAcquisitions = 10.02,
+      vatDueTotal = 10.03,
+      vatReclaimedCurrPeriod = 10.04,
+      vatDueNet = 10.05,
+      totalValueSalesExVAT = 10.06,
+      totalValuePurchasesExVAT = 10.07,
+      totalValueGoodsSuppliedExVAT = 10.08,
+      totalAllAcquisitionsExVAT = 10.09,
+      agentReferenceNumber = Some("XARN1234567")
+    )
+
+    "submission is successful" should {
+
+      val expectedResult = Right(SubmissionSuccessModel(formBundleNumber = "12345"))
+
+      "return a SubmissionModel" in {
+        (mockConnector.submitVatReturn(_: String, _: SubmissionModel)(_: HeaderCarrier, _: ExecutionContext))
+          .expects(*, *, *, *)
+          .returning(Future.successful(expectedResult))
+
+        val result: HttpGetResult[SubmissionSuccessModel] = await(service.submitVatReturn("999999999", submissionModel))
+
+        result shouldBe expectedResult
+      }
+    }
+
+    "submission is unsuccessful" should {
+
+      val expectedResult = Left(UnexpectedJsonFormat)
+
+      "return a HttpError" in {
+        (mockConnector.submitVatReturn(_: String, _: SubmissionModel)(_: HeaderCarrier, _: ExecutionContext))
+          .expects(*, *, *, *)
+          .returning(Future.successful(expectedResult))
+
+        val result: HttpGetResult[SubmissionSuccessModel] = await(service.submitVatReturn("999999999", submissionModel))
+
+        result shouldBe expectedResult
+      }
+    }
+  }
+}
