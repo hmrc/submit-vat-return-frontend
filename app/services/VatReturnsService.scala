48f076f8d4666c32335deef7af3c1bfe7c5dd6d2
Miranda Hawkes <17408278+miranda-hawkes@users.noreply.github.com>
2019-07-25 14:51:58 +0100

    Added nrs submission method to vat returns service

--- app/services/VatReturnsService.scala
+++ app/services/VatReturnsService.scala
@@ -16,11 +16,15 @@
 
 package services
 
+import java.time.LocalDateTime
 import connectors.VatReturnsConnector
 import connectors.httpParsers.ResponseHttpParsers.HttpGetResult
 import javax.inject.{Inject, Singleton}
+import models.nrs._
+import models.nrs.identityData._
 import models.vatReturnSubmission.{SubmissionModel, SubmissionSuccessModel}
 import uk.gov.hmrc.http.HeaderCarrier
+
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
@@ -29,5 +33,54 @@ class VatReturnsService @Inject()(vatReturnsConnector: VatReturnsConnector) {
   def submitVatReturn(vrn: String, model: SubmissionModel)
                      (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpGetResult[SubmissionSuccessModel]] = {
     vatReturnsConnector.submitVatReturn(vrn, model)
+  }
+
+  def nrsSubmission()
+                   (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpGetResult[SuccessModel]] = {
+
+    //TODO: BTAT-6413
+    val identityDataModel = IdentityData(
+      credentials = IdentityCredentials("", ""),
+      confidenceLevel = 0,
+      name = IdentityName("", ""),
+      agentInformation =  IdentityAgentInformation("", "", ""),
+      itmpName = IdentityItmpName("", "", ""),
+      itmpAddress = IdentityItmpAddress("", "", "", ""),
+      loginTimes = IdentityLoginTimes(LocalDateTime.now(), LocalDateTime.now())
+    )
+
+    //TODO: BTAT-6416
+    val receiptDataModel = ReceiptData(
+      EN,
+      Seq(),
+      Declaration("", "", None, declarationConsent = false)
+    )
+
+    //TODO: BTAT-6414
+    val headerData = Map.empty[String, String]
+
+    //TODO: BTAT-6417
+    val metaData = Metadata(
+      businessId = "",
+      notableEvent = "",
+      payloadContentType = "",
+      payloadSha256Checksum = "",
+      userSubmissionTimestamp = LocalDateTime.now(),
+      identityData = identityDataModel,
+      userAuthToken = "",
+      headerData = headerData,
+      searchKeys = SearchKeys("", ""),
+      receiptData = receiptDataModel
+    )
+
+    //TODO: BTAT-6415
+    val payload = ""
+
+    val submissionModel = RequestModel(
+      payload = payload,
+      metadata = metaData
+    )
+
+    vatReturnsConnector.nrsSubmission(submissionModel)
   }
 }
