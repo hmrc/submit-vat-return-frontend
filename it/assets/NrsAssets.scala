aefbf3a6cf74190d040ab8362794c3c709db234a
Sam Raspin <14580184+sraspin@users.noreply.github.com>
2019-08-02 10:56:55 +0100

    [BTAT-6419] - cleaned up NrsAssets

--- it/assets/NrsAssets.scala
+++ it/assets/NrsAssets.scala
@@ -16,60 +16,19 @@
 
 package assets
 
-import java.time.LocalDate
-
 import base.BaseISpec
-import models.nrs.identityData._
-import models.{ConfirmSubmissionViewModel, SubmitVatReturnModel}
-import models.nrs._
-import play.api.libs.json.Json
-import play.api.test.FakeRequest
-import utils.HashUtil
 
 trait NrsAssets extends BaseISpec {
 
-  lazy val date = LocalDate.parse("2019-12-25")
-
-  lazy val submitModel: SubmitVatReturnModel =
-    SubmitVatReturnModel(10.01, 10.02, 10.03, 10.04, 10.05, 10.06, 10.07, 10.08, 10.09, false, date, date, date)
-
-  lazy val viewModel = ConfirmSubmissionViewModel(submitModel, "18AA", Some(" "))
-  lazy val html = views.html.confirm_submission(viewModel, false)(FakeRequest("", ""), messages, appConfig, user)
-  lazy val payloadHash = HashUtil.encode(html.body)
-  lazy val sha256Checksum = HashUtil.getHash(html.body)
-
   lazy val businessId = "vat"
-
   lazy val notableEvent = "vat-return"
-
   lazy val payloadContentType = "text\\/html"
-
   lazy val timestamp = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}Z"
-
-
-  lazy val credentials = IdentityCredentials("", "")
   lazy val confidenceLevel = 0
-  lazy val name = IdentityName("", "")
-  lazy val agentInformation = IdentityAgentInformation("", "", "")
-  lazy val itmpName = IdentityItmpName("", "", "")
-  lazy val itmpAddress = IdentityItmpAddress("", "", "", "")
 
-  lazy val searchKeys = SearchKeys("999999999", "18AA")
-
-  lazy val receiptData = ReceiptData(
-    EN,
-    Seq(),
-    Declaration(
-      "", "", None, false
-    )
-  )
-
-  lazy val declaration = Declaration(
-    "", "", None, false
-  )
-
-  lazy val jaason =
-    """\{"payload":".*?","metadata":\{"businessId":"vat","notableEvent":"vat-return","payloadContentType":"text\/html","payloadSha256Checksum":".*?"
+  lazy val jaason: String =
+    """\{"payload":".*?","metadata":\{"businessId":"""" + businessId + """","notableEvent":"""" + notableEvent +
+      """","payloadContentType":"""" + payloadContentType + """","payloadSha256Checksum":".*?"
       |,"userSubmissionTimestamp":"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z","identityData":\{"credentials":\{"providerId":"","providerType":""\},
       |"confidenceLevel":0,"name":\{"name":"","lastName":""\},"agentInformation":\{"agentCode":"","agentFriendlyName":"","agentId":""\},"itmpName":\{
       |"givenName":"","middleName":"","familyName":""\},"itmpAddress":\{"line1":"","postCode":"","countryName":"","countryCode":""\},"loginTimes":\{
