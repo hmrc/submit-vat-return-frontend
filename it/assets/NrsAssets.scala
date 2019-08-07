e64e345fc4893caf0e469d4f20cf5aaf0b947485
Tom Dudley <TomDudley92@users.noreply.github.com>
2019-08-07 14:43:52 +0100

    refactored regex for nrs full submission json

--- it/assets/NrsAssets.scala
+++ it/assets/NrsAssets.scala
@@ -20,53 +20,52 @@ import base.BaseISpec
 
 trait NrsAssets extends BaseISpec {
 
-  lazy val businessId = "vat"
-  lazy val notableEvent = "vat-return"
-  lazy val payloadContentType = "text\\/html"
+  val businessId = "vat"
+  val notableEvent = "vat-return"
+  val payloadContentType = "text\\/html"
 
-  lazy val nrsFullSubmissionJson: String =
+  val nrsFullSubmissionJson: String =
     """\{"payload":".*?","metadata":\{"businessId":"""" + businessId + """","notableEvent":"""" + notableEvent +
-      """","payloadContentType":"""" + payloadContentType + """","payloadSha256Checksum":".*?"
-      |,"userSubmissionTimestamp":"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z",
-      |"identityData":\
-      |{"internalId":".*?",
-      |"externalId":".*?",
-      |"agentCode":".*?",
-      |"credentials":\
-      |{"providerId":".*?","providerType":".*?"\},
-      |"confidenceLevel":.*?,
-      |"nino":".*?",
-      |"saUtr":".*?",
-      |"name":\{"name":".*?","lastName":".*?"\},
-      |"dateOfBirth":"\d{4}-\d{2}-\d{2}",
-      |"email":".*?",
-      |"agentInformation":\{"agentId":".*?","agentCode":".*?","agentFriendlyName":".*?"\},
-      |"groupIdentifier":".*?",
-      |"credentialRole":".*?",
-      |"mdtpInformation":\{"deviceId":".*?","sessionId":".*?"\},
-      |"itmpName":\{"givenName":".*?","middleName":".*?","familyName":".*?"\},
-      |"itmpDateOfBirth":"\d{4}-\d{2}-\d{2}",
-      |"itmpAddress":\{"line1":".*?","postCode":".*?","countryName":".*?","countryCode":".*?"\},
-      |"affinityGroup":".*?",
-      |"credentialStrength":".*?",
-      |"loginTimes":\{"currentLogin":"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z",
-      |"previousLogin":"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z"\}\},
-      |"userAuthToken":"Bearer 1234",
-      |"headerData":\{"Csrf-Token":"nocheck","X-Request-ID":"govuk-tax-.*?","X-Request-Timestamp":"\d*","Content-Length":"2","Accept":
-      |"\*\/\*","Content-Type":"application\/json","Cookie":".*?","User-Agent":"AHC\/2\.0","Host":"localhost:19001"\},"searchKeys":\{"vrn":"999999999",
-      |"periodKey":"18AA"\},
-      |"receiptData":\{"language":"en","checkYourAnswersSections":\[\{"title":"Your VAT Return",
-      |"data":\[
-      |\{"questionId":"box1","question":"VAT you charged on sales and other supplies","answer":".*?"\},
-      |\{"questionId":"box2","question":"VAT you owe on goods purchased from EC countries and brought into the UK","answer":".*?"\},
-      |\{"questionId":"box3","question":"VAT you owe before deductions \(this is the total of box 1 and 2\)","answer":".*?"\},
-      |\{"questionId":"box4","question":"VAT you have claimed back","answer":".*?"\},
-      |\{"questionId":"box5","question":"Return total","answer":".*?"\},
-      |\{"questionId":"box6","question":"Total value of sales and other supplies, excluding VAT","answer":".*?"\},
-      |\{"questionId":"box7","question":"Total value of purchases and other expenses, excluding VAT","answer":".*?"\},
-      |\{"questionId":"box8","question":"Total value of supplied goods to EC countries and related costs \(excluding VAT\)","answer":".*?"\},
-      |\{"questionId":"box9","question":"Total value of goods purchased from EC countries and brought into the UK, as well as any related costs \(excluding VAT\)","answer":".*?"\}\]\}\],
-      |"declaration":\{"declarationText":".*?","declarationName":".*?",
-      |"declarationConsent":true\}\}\}\}""".stripMargin.replace("\n", "")
-
+      """","payloadContentType":"""" + payloadContentType +
+      """","payloadSha256Checksum":".*?",
+        |"userSubmissionTimestamp":"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z",
+        |"identityData":\
+        |{"internalId":".*?",
+        |"externalId":".*?",
+        |"agentCode":".*?",
+        |"credentials":\{"providerId":".*?","providerType":".*?"\},
+        |"confidenceLevel":.*?,
+        |"nino":".*?",
+        |"saUtr":".*?",
+        |"name":\{"name":".*?","lastName":".*?"\},
+        |"dateOfBirth":"\d{4}-\d{2}-\d{2}",
+        |"email":".*?",
+        |"agentInformation":\{"agentId":".*?","agentCode":".*?","agentFriendlyName":".*?"\},
+        |"groupIdentifier":".*?",
+        |"credentialRole":".*?",
+        |"mdtpInformation":\{"deviceId":".*?","sessionId":".*?"\},
+        |"itmpName":\{"givenName":".*?","middleName":".*?","familyName":".*?"\},
+        |"itmpDateOfBirth":"\d{4}-\d{2}-\d{2}",
+        |"itmpAddress":\{"line1":".*?","postCode":".*?","countryName":".*?","countryCode":".*?"\},
+        |"affinityGroup":".*?",
+        |"credentialStrength":".*?",
+        |"loginTimes":\{"currentLogin":"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z",
+        |"previousLogin":"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z"\}
+        |\},
+        |"userAuthToken":"Bearer 1234",
+        |"headerData":\{"Csrf-Token":"nocheck","X-Request-ID":"govuk-tax-.*?","X-Request-Timestamp":"\d*","Content-Length":"2","Accept":
+        |"\*\/\*","Content-Type":"application\/json","Cookie":".*?","User-Agent":"AHC\/2\.0","Host":"localhost:19001"\},
+        |"searchKeys":\{"vrn":"999999999","periodKey":"18AA"\},
+        |"receiptData":\{"language":"en","checkYourAnswersSections":\[\{"title":"Your VAT Return",
+        |"data":\[
+        |\{"questionId":"box1","question":".*?","answer":".*?"\},
+        |\{"questionId":"box2","question":".*?","answer":".*?"\},
+        |\{"questionId":"box3","question":".*?","answer":".*?"\},
+        |\{"questionId":"box4","question":".*?","answer":".*?"\},
+        |\{"questionId":"box5","question":".*?","answer":".*?"\},
+        |\{"questionId":"box6","question":".*?","answer":".*?"\},
+        |\{"questionId":"box7","question":".*?","answer":".*?"\},
+        |\{"questionId":"box8","question":".*?","answer":".*?"\},
+        |\{"questionId":"box9","question":".*?","answer":".*?"\}\]\}\],
+        |"declaration":\{"declarationText":".*?","declarationName":".*?","declarationConsent":true\}\}\}\}""".stripMargin.replace("\n", "")
 }
