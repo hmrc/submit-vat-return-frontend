71fe3a8d7d1722ea46549627ab24f1fd285e62a0
JoshuaGomersallHMRC <53816372+JoshuaGomersallHMRC@users.noreply.github.com>
2019-11-12 16:03:07 +0000

    BTAT-7131 Added service info connector and service

---
+++ test/connectors/ServiceInfoPartialConnectorSpec.scala
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import play.api.http.Status
+import play.twirl.api.Html
+import uk.gov.hmrc.http.{HeaderCarrier, HttpReads}
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
+
+import scala.concurrent.{ExecutionContext, Future}
+import config.VatHeaderCarrierForPartialsConverter
+import play.api.mvc.AnyContentAsEmpty
+import play.api.test.FakeRequest
+import uk.gov.hmrc.play.partials.HtmlPartial
+import uk.gov.hmrc.play.partials.HtmlPartial.{Failure, Success}
+import views.templates.TemplateBaseSpec
+
+class ServiceInfoPartialConnectorSpec extends TemplateBaseSpec {
+
+  val header: VatHeaderCarrierForPartialsConverter = injector.instanceOf[VatHeaderCarrierForPartialsConverter]
+  implicit val ec: ExecutionContext = injector.instanceOf[ExecutionContext]
+  val validHtml = Html("<nav>BTA lINK</nav>")
+
+  private trait Test {
+    implicit lazy val fakeRequest: FakeRequest[AnyContentAsEmpty.type] = FakeRequest()
+    val result :Future[HtmlPartial] = Future.successful(Success(None,validHtml))
+    val httpClient: HttpClient = mock[HttpClient]
+    lazy val connector: ServiceInfoPartialConnector = {
+
+      (httpClient.GET[HtmlPartial](_: String)(_: HttpReads[HtmlPartial],_: HeaderCarrier,_: ExecutionContext))
+        .stubs(*,*,*,*)
+        .returns(result)
+      new ServiceInfoPartialConnector(httpClient, header)(messagesApi, mockAppConfig)
+    }
+  }
+
+  "ServiceInfoPartialConnector" should {
+    "generate the correct url" in new Test {
+      connector.btaUrl shouldBe "/business-account/partial/service-info"
+    }
+  }
+
+  "getServiceInfoPartial" when{
+    "a connectionExceptionsAsHtmlPartialFailure error is returned" should {
+      "return the fall back partial" in new Test{
+        override val result: Future[Failure] = Future.successful(Failure(Some(Status.GATEWAY_TIMEOUT)))
+        await(connector.getServiceInfoPartial()) shouldBe views.html.templates.btaNavigationLinks()
+      }
+    }
+
+    "an unexpected Exception is returned" should {
+      "return the fall back partial" in new Test{
+        override val result: Future[Failure] = Future.successful(Failure(Some(Status.INTERNAL_SERVER_ERROR)))
+        await(connector.getServiceInfoPartial()) shouldBe views.html.templates.btaNavigationLinks()
+      }
+    }
+
+    "a successful response is returned" should {
+      "return the Bta partial" in new Test{
+        await(connector.getServiceInfoPartial()) shouldBe validHtml
+      }
+    }
+  }
+}
--- test/mocks/MockConfig.scala
+++ test/mocks/MockConfig.scala
@@ -63,8 +63,8 @@ class MockConfig(implicit val runModeConfiguration: Configuration) extends AppCo
   )
 
   override val viewSubmittedReturnUrl: String = "/submittedreturns/year/periodkey"
-  override val btaBaseUrl: String = "/home"
-  override val btaHomeUrl: String = "/adawd"
+  override val btaBaseUrl: String = ""
+  override val btaHomeUrl: String = "/home"
   override val btaMessagesUrl: String = "/messages"
   override val btaManageAccountUrl: String = "/manageAccountUrl"
   override val btaHelpAndContactUrl: String = "/helpandcontact"
