06d8e1456c5bb5e43b9734b5d27046740fda031d
william-hatzar <22661741+william-hatzar@users.noreply.github.com>
2021-10-07 11:59:17 +0100

    Removal of btaHome from appconf

--- test/mocks/MockConfig.scala
+++ test/mocks/MockConfig.scala
@@ -62,6 +62,4 @@ class MockConfig(implicit val runModeConfiguration: Configuration) extends AppCo
 
   override val platformHost: String = "platform.host"
   override val directDebitInterruptUrl: String = "/directDebitUrl"
-
-  override val btaHomeUrl: String = "/bta-home"
 }
